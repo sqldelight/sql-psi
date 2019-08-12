35b7f0761c06c61f7788daa2a164cd8fe7a22b63
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2019-08-12 06:36:39 -0400

    Fixed synthesized column handling when joining tables (#40)
    * Removed invalid synthesized column "_oid_" and added "_rowid_"
    
    * Fixed synthesized columns not getting included in joined tables
    
    * Revert "Fixed synthesized columns not getting included in joined tables"
    
    This reverts commit ebd4073387ea1c40ec518d6f1b6a4a5b296c755b.
    
    * Fixed synthesized columns not getting included in joined tables
    
    * Change PsiNamedElement to PsiNameIdentifierOwner (#36)
    
    MemberInplaceRenameHandler uses it internally, for difference between them see: https://intellij-support.jetbrains.com/hc/en-us/community/posts/206103209/comments/206084085
    
    * Added checks for invalid MATCH operator usage
    
    * Reduce code verbosity in isMatchUsageErrorOnRegularColumn()

---
+++ core/src/test/fixtures/match-operator/Test.s
@@ -0,0 +1,72 @@
+CREATE TABLE book(
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  author TEXT NOT NULL,
+  publisher TEXT NOT NULL
+);
+
+CREATE VIRTUAL TABLE book_fts USING fts4(content TEXT);
+
+-- Expected failure: MATCH can't be used on a regular table
+SELECT * FROM book WHERE book MATCH ?;
+
+-- Expected failure: MATCH can't be used on a column within a regular table
+SELECT * FROM book WHERE title MATCH ?;
+
+SELECT * FROM book_fts WHERE content MATCH ?;
+
+SELECT book.*
+FROM book
+INNER JOIN book_fts ON docid = book.id
+WHERE book_fts MATCH ?1;
+
+SELECT book.*
+FROM book_fts
+LEFT JOIN book ON docid = book.id
+WHERE book_fts MATCH ?1;
+
+SELECT book.*
+FROM book_fts
+LEFT JOIN book ON docid = book.id
+WHERE content MATCH ?1;
+
+-- Expected failure: MATCH can't be used when an FTS table is on the right of a left join
+SELECT book.*
+FROM book
+LEFT JOIN book_fts ON docid = book.id
+WHERE book_fts MATCH ?1;
+
+-- Expected failure: MATCH can't be used when an FTS table is on the right of a left join
+SELECT book.*
+FROM book
+LEFT JOIN book_fts ON docid = book.id
+WHERE content MATCH ?1;
+
+-- Expected failure: MATCH can't be used on a regular table
+SELECT book.*
+FROM book_fts
+JOIN book ON docid = book.id
+WHERE title MATCH ?1;
+
+SELECT book.*
+FROM book_fts
+JOIN book ON docid = book.id
+WHERE book_fts MATCH title;
+
+-- Expected failure: MATCH can't be used on a bind expression
+SELECT book.*
+FROM book_fts
+JOIN book ON docid = book.id
+WHERE :bind MATCH :searchText;
+
+-- Expected failure: MATCH can't be used on a literal expression
+SELECT book.*
+FROM book_fts
+JOIN book ON docid = book.id
+WHERE 'literal' MATCH :searchText;
+
+-- Expected failure: MATCH can't be used on a CASE statement
+SELECT book.*
+FROM book_fts
+JOIN book ON docid = book.id
+WHERE (CASE WHEN book.id = 1 THEN book_fts ELSE book_fts.content END) MATCH :searchText;
