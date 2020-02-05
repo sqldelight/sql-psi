cc8da881f13fe3102fcb39e62098916922bd7d5b
Alec Strong <astrong@squareup.com>
2020-02-05 00:02:32 -0500

    Add sqlite 3.24 grammar

--- core/src/main/kotlin/com/alecstrong/sqlite/psi/core/sqlite_3_24/psi/mixins/UpsertClauseMixin.kt
+++ core/src/main/kotlin/com/alecstrong/sqlite/psi/core/sqlite_3_24/psi/mixins/UpsertClauseMixin.kt
@@ -1,27 +1,34 @@
-package com.alecstrong.sqlite.psi.core.psi.mixins
+package com.alecstrong.sqlite.psi.core.sqlite_3_24.psi.mixins
 
 import com.alecstrong.sqlite.psi.core.psi.*
 import com.alecstrong.sqlite.psi.core.psi.SqliteCompositeElementImpl
+import com.alecstrong.sqlite.psi.core.psi.mixins.InsertStmtMixin
+import com.alecstrong.sqlite.psi.core.psi.mixins.SingleRow
+import com.alecstrong.sqlite.psi.core.sqlite_3_24.psi.UpsertClause
+import com.alecstrong.sqlite.psi.core.sqlite_3_24.psi.UpsertConflictTarget
+import com.alecstrong.sqlite.psi.core.sqlite_3_24.psi.UpsertDoUpdate
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 
 internal abstract class UpsertClauseMixin(
         node: ASTNode
 ) : SqliteCompositeElementImpl(node),
-        SqliteUpsertClause {
+    UpsertClause {
 
     override fun queryAvailable(child: PsiElement): Collection<QueryElement.QueryResult> {
         val insertStmt = (this.parent as InsertStmtMixin)
         val tableName = insertStmt.tableName
         val table = tablesAvailable(this).first { it.tableName.name == tableName.name }.query
 
-        if (child is SqliteUpsertConflictTarget) {
+        if (child is UpsertConflictTarget) {
             return super.queryAvailable(child)
         }
 
-        if (child is SqliteUpsertDoUpdate) {
+        if (child is UpsertDoUpdate) {
             val excludedTable = QueryElement.QueryResult(
-                    SingleRow(tableName, "excluded"), table.columns,
+                SingleRow(
+                    tableName, "excluded"
+                ), table.columns,
                     synthesizedColumns = table.synthesizedColumns)
 
             val available = arrayListOf(excludedTable)
