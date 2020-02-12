594eddca55bfa8a3acc7e7d8a70356a8f4502f08
Alec Strong <astrong@squareup.com>
2020-02-12 12:52:40 -0500

    Add support for the MySQL dialect

--- core/src/main/kotlin/com/alecstrong/sql/psi/core/DialectPresets.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/DialectPresets.kt
@@ -1,19 +1,28 @@
 package com.alecstrong.sql.psi.core
 
+import com.alecstrong.sql.psi.core.mysql.MySqlParserUtil
+import com.alecstrong.sql.psi.core.sqlite_3_18.SqliteParserUtil as Sqlite_3_18Util
 import com.alecstrong.sql.psi.core.sqlite_3_24.SqliteParserUtil as Sqlite_3_24Util
 
 enum class DialectPreset {
-  SQLITE, SQLITE_3_24;
+  SQLITE_3_18, SQLITE_3_24, MYSQL;
 
   fun setup() {
-    when (this) {
-      SQLITE -> {
+    val exhaustive = when (this) {
+      SQLITE_3_18 -> {
         SqlParserUtil.reset()
+        Sqlite_3_18Util.reset()
+        Sqlite_3_18Util.overrideSqlParser()
       }
       SQLITE_3_24 -> {
-        SqlParserUtil.reset()
+        SQLITE_3_18.setup()
         Sqlite_3_24Util.reset()
         Sqlite_3_24Util.overrideSqlParser()
+      }
+      MYSQL -> {
+        SqlParserUtil.reset()
+        MySqlParserUtil.reset()
+        MySqlParserUtil.overrideSqlParser()
       }
     }
   }
