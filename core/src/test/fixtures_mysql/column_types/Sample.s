3607273c8b9616277394295937b3067bbe91bc12
Alec Strong <AlecStrong@users.noreply.github.com>
2020-02-12 14:21:21 -0500

    Add support for the MySQL dialect (#54)

---
+++ core/src/test/fixtures_mysql/column_types/Sample.s
@@ -0,0 +1,23 @@
+CREATE TABLE all_types(
+  some_int INTEGER,
+  same_small_int SMALLINT,
+  some_decimal DECIMAL,
+  some_numeric NUMERIC,
+  some_float FLOAT,
+  some_real REAL,
+  some_double_precision DOUBLE PRECISION,
+  some_date DATE,
+  some_time TIME,
+  some_datetime DATETIME,
+  some_timestamp TIMESTAMP,
+  some_year YEAR,
+  some_char CHAR,
+  some_varchar VARCHAR(16),
+  some_text TEXT,
+  some_enum ENUM,
+  some_set SET,
+  some_varbinary VARBINARY(8),
+  some_blob BLOB(8, 8),
+  some_binary BINARY,
+  some_json JSON
+);
