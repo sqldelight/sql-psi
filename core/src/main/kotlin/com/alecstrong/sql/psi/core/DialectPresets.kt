e0e24dcc099b48768a33aef8a0e5df58d40b1736
Marius Volkhart <marius@volkhart.com>
2020-05-20 16:00:56 +0200

    Add support for the HSQLDB dialect (#61)
    Co-authored-by: Alec Strong <AlecStrong@users.noreply.github.com>

--- core/src/main/kotlin/com/alecstrong/sql/psi/core/DialectPresets.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/DialectPresets.kt
@@ -1,12 +1,13 @@
 package com.alecstrong.sql.psi.core
 
+import com.alecstrong.sql.psi.core.hsql.HsqlParserUtil
 import com.alecstrong.sql.psi.core.mysql.MySqlParserUtil
 import com.alecstrong.sql.psi.core.postgresql.PostgreSqlParserUtil
 import com.alecstrong.sql.psi.core.sqlite_3_18.SqliteParserUtil as Sqlite_3_18Util
 import com.alecstrong.sql.psi.core.sqlite_3_24.SqliteParserUtil as Sqlite_3_24Util
 
 enum class DialectPreset {
-  SQLITE_3_18, SQLITE_3_24, MYSQL, POSTGRESQL;
+  SQLITE_3_18, SQLITE_3_24, MYSQL, POSTGRESQL, HSQL;
 
   fun setup() {
     val exhaustive = when (this) {
@@ -24,6 +25,11 @@ enum class DialectPreset {
         SqlParserUtil.reset()
         MySqlParserUtil.reset()
         MySqlParserUtil.overrideSqlParser()
+      }
+      HSQL -> {
+        SqlParserUtil.reset()
+        HsqlParserUtil.reset()
+        HsqlParserUtil.overrideSqlParser()
       }
       POSTGRESQL -> {
         SqlParserUtil.reset()
