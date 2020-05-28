49d9e9add55093311f8ef10607f5eaa0176c053b
Jeff Gulbronson <jgulbronson@gmail.com>
2020-05-28 07:54:13 -0400

    Fix code based on Alec's feedback

--- core/src/test/fixtures_mysql/create_table_schema/UniqueKey.s
+++ core/src/test/fixtures_mysql/create_table_schema/UniqueKey.s
@@ -4,7 +4,7 @@ CREATE TABLE animals (
     UNIQUE KEY unq_name (name)
 );
 
-CREATE TABLE animals (
+CREATE TABLE animals2 (
     id BIGINT AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     UNIQUE INDEX unq_name (name)
