7d03e1f8104c387b3336a83ddbccdac4d48f3e40
Alec Strong <astrong@squareup.com>
2020-05-31 23:01:40 -0400

    Add a test for left joins and group by

--- core/src/test/kotlin/com/alecstrong/sql/psi/core/NullabilityTest.kt
+++ core/src/test/kotlin/com/alecstrong/sql/psi/core/NullabilityTest.kt
@@ -32,4 +32,43 @@ class NullabilityTest {
     assertThat(projection[1].nullable).isTrue()
     assertThat(projection[2].nullable).isTrue()
   }
+
+  @Test fun leftJoinGroupBy() {
+    val file = compileFile("""
+      |CREATE TABLE target (
+      |  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+      |  coacheeId INTEGER NOT NULL,
+      |  name TEXT NOT NULL
+      |);
+      |
+      |CREATE TABLE challengeTarget (
+      |  targetId INTEGER NOT NULL,
+      |  challengeId INTEGER NOT NULL
+      |);
+      |
+      |CREATE TABLE challenge (
+      |  id INTEGER NOT NULL,
+      |  cancelledAt INTEGER,
+      |  emoji TEXT
+      |);
+      |
+      |SELECT target.id AS id, target.name AS name, challenge.emoji
+      |  FROM target
+      |  LEFT JOIN challengeTarget
+      |    ON challengeTarget.targetId = target.id
+      |  LEFT JOIN challenge
+      |    ON challengeTarget.challengeId = challenge.id AND challenge.cancelledAt IS NULL
+      |  WHERE target.coacheeId = ?
+      |  GROUP BY 1
+      |  ORDER BY target.name COLLATE NOCASE ASC
+      |;
+    """.trimMargin())
+
+    val select = file.sqlStmtList!!.stmtList.mapNotNull { it.compoundSelectStmt }.single()
+    val projection = select.queryExposed().flatMap { it.columns }
+
+    assertThat(projection[0].nullable).isFalse()
+    assertThat(projection[1].nullable).isFalse()
+    assertThat(projection[2].nullable).isTrue()
+  }
 }
