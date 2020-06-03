522d8d4a344cb7404dabf80f66d5df9c43689bd4
Alec Strong <AlecStrong@users.noreply.github.com>
2020-06-03 16:15:05 -0400

    Support ALTER TABLE CHANGE (#103)

--- core/src/main/kotlin/com/alecstrong/sql/psi/core/psi/AlterTableApplier.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/psi/AlterTableApplier.kt
@@ -2,6 +2,10 @@ package com.alecstrong.sql.psi.core.psi
 
 import com.alecstrong.sql.psi.core.AnnotationException
 
+internal interface AlterTableApplier {
+  fun applyTo(lazyQuery: LazyQuery): LazyQuery
+}
+
 internal fun LazyQuery.withAlterStatement(alter: SqlAlterTableStmt): LazyQuery {
   return alter.alterTableRulesList.fold(this, { lazyQuery, alterTableRules ->
     // Add column.
@@ -15,13 +19,17 @@ internal fun LazyQuery.withAlterStatement(alter: SqlAlterTableStmt): LazyQuery {
     }
 
     // Rename table.
-    alterTableRules.alterTableRenameTable?.let { renametable ->
+    alterTableRules.alterTableRenameTable?.let { renameTable ->
       return@fold LazyQuery(
-          tableName = renametable.newTableName,
+          tableName = renameTable.newTableName,
           query = {
-            lazyQuery.query.copy(table = renametable.newTableName)
+            lazyQuery.query.copy(table = renameTable.newTableName)
           }
       )
+    }
+
+    (alterTableRules.firstChild as? AlterTableApplier)?.let {
+      return it.applyTo(lazyQuery)
     }
 
     throw AnnotationException("Unhandled alter rule", alterTableRules)
