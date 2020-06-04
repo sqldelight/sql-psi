12db5d4cebd1c3339ab77c1e68829b4a9dee5c7c
Alec Strong <AlecStrong@users.noreply.github.com>
2020-06-03 20:55:18 -0400

    Support the placement clause of some alter column statements (#106)

--- core/src/test/kotlin/com/alecstrong/sql/psi/core/TablesExposedTest.kt
+++ core/src/test/kotlin/com/alecstrong/sql/psi/core/TablesExposedTest.kt
@@ -1,9 +1,23 @@
 package com.alecstrong.sql.psi.core
 
 import com.google.common.truth.Truth.assertThat
+import java.io.File
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class TablesExposedTest {
+  @Before
+  fun before() {
+    File("build/tmp").deleteRecursively()
+  }
+
+  @After
+  fun after() {
+    SqlParserUtil.reset()
+    File("build/tmp").deleteRecursively()
+  }
+
   @Test fun `tables works correctly for include all`() {
     compileFile("""
       |CREATE TABLE test1 (
