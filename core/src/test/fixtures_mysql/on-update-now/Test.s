66d67013796d71c6bfa09232b1f7acfb9c529e05
Alec Strong <AlecStrong@users.noreply.github.com>
2020-06-10 10:52:49 -0400

    Update grammar composer and constrain identifiers more (#120)

---
+++ core/src/test/fixtures_mysql/on-update-now/Test.s
@@ -0,0 +1,9 @@
+CREATE TABLE stuff (
+  id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  use_after smallint NOT NULL,
+  updated_by varchar(255) NOT NULL,
+  created_at timestamp(3) NOT NULL DEFAULT NOW(3),
+  updated_at timestamp(3) NOT NULL DEFAULT NOW(3) ON UPDATE NOW(3),
+  KEY idx_credentials_updated_at(updated_at),
+  UNIQUE (use_after)
+);
