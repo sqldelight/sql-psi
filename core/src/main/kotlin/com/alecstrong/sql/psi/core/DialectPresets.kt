27f55697eed208984872b19d9ef43ea21d0e4859
Alec Strong <AlecStrong@users.noreply.github.com>
2020-06-19 22:22:27 -0400

    Support alter table rename column (#125)
    * Support alter table rename column
    
    * Add support for ALTER TABLE foo RENAME COLUMN
    
    https://github.com/cashapp/sqldelight/issues/1505
    (cherry picked from commit 513f0d4f5665862eb91233aef876b82ecce1892a)
    
    * Complete support of rename column
    
    Co-authored-by: Angus Holder <aholder97@gmail.com>

--- core/src/main/kotlin/com/alecstrong/sql/psi/core/DialectPresets.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/DialectPresets.kt
@@ -5,9 +5,10 @@ import com.alecstrong.sql.psi.core.mysql.MySqlParserUtil
 import com.alecstrong.sql.psi.core.postgresql.PostgreSqlParserUtil
 import com.alecstrong.sql.psi.core.sqlite_3_18.SqliteParserUtil as Sqlite_3_18Util
 import com.alecstrong.sql.psi.core.sqlite_3_24.SqliteParserUtil as Sqlite_3_24Util
+import com.alecstrong.sql.psi.core.sqlite_3_25.SqliteParserUtil as Sqlite_3_25Util
 
 enum class DialectPreset {
-  SQLITE_3_18, SQLITE_3_24, MYSQL, POSTGRESQL, HSQL;
+  SQLITE_3_18, SQLITE_3_24, SQLITE_3_25, MYSQL, POSTGRESQL, HSQL;
 
   fun setup() {
     val exhaustive = when (this) {
@@ -20,6 +21,11 @@ enum class DialectPreset {
         SQLITE_3_18.setup()
         Sqlite_3_24Util.reset()
         Sqlite_3_24Util.overrideSqlParser()
+      }
+      SQLITE_3_25 -> {
+        SQLITE_3_24.setup()
+        Sqlite_3_25Util.reset()
+        Sqlite_3_25Util.overrideSqlParser()
       }
       MYSQL -> {
         SqlParserUtil.reset()
