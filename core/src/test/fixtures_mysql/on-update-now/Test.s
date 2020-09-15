a3d7c2b6cdbb28b69b724a74a4927e5e564277c9
Alec Strong <AlecStrong@users.noreply.github.com>
2020-09-14 21:52:29 -0400

    Support NOW with no literal (#180)

--- core/src/test/fixtures_mysql/on-update-now/Test.s
+++ core/src/test/fixtures_mysql/on-update-now/Test.s
@@ -4,6 +4,7 @@ CREATE TABLE stuff (
   updated_by varchar(255) NOT NULL,
   created_at timestamp(3) NOT NULL DEFAULT NOW(3),
   updated_at timestamp(3) NOT NULL DEFAULT NOW(3) ON UPDATE NOW(3),
+  other_at timestamp(3) NOT NULL DEFAULT NOW() ON UPDATE NOW(),
   KEY idx_credentials_updated_at(updated_at),
   UNIQUE (use_after)
 );
