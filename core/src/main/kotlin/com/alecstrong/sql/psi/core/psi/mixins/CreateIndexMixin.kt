5d262e3a474379d167929528245a10a16269ad2c
Alec Strong <AlecStrong@users.noreply.github.com>
2020-09-17 13:18:35 -0400

    Use schema contributor interface for indexes (#182)

--- core/src/main/kotlin/com/alecstrong/sql/psi/core/psi/mixins/CreateIndexMixin.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/psi/mixins/CreateIndexMixin.kt
@@ -2,6 +2,8 @@ package com.alecstrong.sql.psi.core.psi.mixins
 
 import com.alecstrong.sql.psi.core.SqlAnnotationHolder
 import com.alecstrong.sql.psi.core.psi.QueryElement.QueryResult
+import com.alecstrong.sql.psi.core.psi.Schema
+import com.alecstrong.sql.psi.core.psi.SchemaContributor
 import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
 import com.alecstrong.sql.psi.core.psi.SqlCreateIndexStmt
 import com.intellij.lang.ASTNode
@@ -10,7 +12,13 @@ import com.intellij.psi.PsiElement
 internal abstract class CreateIndexMixin(
   node: ASTNode
 ) : SqlCompositeElementImpl(node),
-    SqlCreateIndexStmt {
+    SqlCreateIndexStmt,
+    SchemaContributor {
+  override fun modifySchema(schema: Schema) {
+    val indexes = schema.forType<SqlCreateIndexStmt>()
+    indexes.putValue(indexName.text, this)
+  }
+
   override fun queryAvailable(child: PsiElement): Collection<QueryResult> {
     if (child in indexedColumnList || child == expr) {
       return listOfNotNull(
@@ -21,7 +29,7 @@ internal abstract class CreateIndexMixin(
   }
 
   override fun annotate(annotationHolder: SqlAnnotationHolder) {
-    if (containingFile.indexes(this).any { it != this && it.indexName.text == indexName.text }) {
+    if (containingFile.schema<SqlCreateIndexStmt>(this).any { it != this && it.indexName.text == indexName.text }) {
       annotationHolder.createErrorAnnotation(indexName, "Duplicate index name ${indexName.text}")
     }
     super.annotate(annotationHolder)
--- core/src/main/kotlin/com/alecstrong/sql/psi/core/psi/mixins/DropIndexMixin.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/psi/mixins/DropIndexMixin.kt
@@ -1,17 +1,28 @@
 package com.alecstrong.sql.psi.core.psi.mixins
 
 import com.alecstrong.sql.psi.core.SqlAnnotationHolder
+import com.alecstrong.sql.psi.core.psi.Schema
+import com.alecstrong.sql.psi.core.psi.SchemaContributor
 import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
+import com.alecstrong.sql.psi.core.psi.SqlCreateIndexStmt
 import com.alecstrong.sql.psi.core.psi.SqlDropIndexStmt
 import com.intellij.lang.ASTNode
 
 internal abstract class DropIndexMixin(
   node: ASTNode
 ) : SqlCompositeElementImpl(node),
-    SqlDropIndexStmt {
+    SqlDropIndexStmt,
+    SchemaContributor {
+  override fun modifySchema(schema: Schema) {
+    indexName?.let { indexName ->
+      val indexes = schema.forType<SqlCreateIndexStmt>()
+      indexes.remove(indexName.text)
+    }
+  }
+
   override fun annotate(annotationHolder: SqlAnnotationHolder) {
     indexName?.let { indexName ->
-      if (containingFile.indexes(this).none { it != this && it.indexName.text == indexName.text }) {
+      if (containingFile.schema<SqlCreateIndexStmt>(this).none { it != this && it.indexName.text == indexName.text }) {
         annotationHolder.createErrorAnnotation(indexName, "No index found with name ${indexName.text}")
       }
     }
