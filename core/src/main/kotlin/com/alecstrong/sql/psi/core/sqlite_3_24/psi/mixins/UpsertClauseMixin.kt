d55f76ce0a88942310cee73e80f6a4ac008485ec
Veyndan Stuart <veyndan@gmail.com>
2020-10-03 02:27:43 +0200

    Prefix SQLite to generated SQLite psi files

--- core/src/main/kotlin/com/alecstrong/sql/psi/core/sqlite_3_24/psi/mixins/UpsertClauseMixin.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/sqlite_3_24/psi/mixins/UpsertClauseMixin.kt
@@ -4,27 +4,27 @@ import com.alecstrong.sql.psi.core.psi.QueryElement
 import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
 import com.alecstrong.sql.psi.core.psi.mixins.InsertStmtMixin
 import com.alecstrong.sql.psi.core.psi.mixins.SingleRow
-import com.alecstrong.sql.psi.core.sqlite_3_24.psi.UpsertClause
-import com.alecstrong.sql.psi.core.sqlite_3_24.psi.UpsertConflictTarget
-import com.alecstrong.sql.psi.core.sqlite_3_24.psi.UpsertDoUpdate
+import com.alecstrong.sql.psi.core.sqlite_3_24.psi.SqliteUpsertClause
+import com.alecstrong.sql.psi.core.sqlite_3_24.psi.SqliteUpsertConflictTarget
+import com.alecstrong.sql.psi.core.sqlite_3_24.psi.SqliteUpsertDoUpdate
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 
 internal abstract class UpsertClauseMixin(
   node: ASTNode
 ) : SqlCompositeElementImpl(node),
-    UpsertClause {
+    SqliteUpsertClause {
 
     override fun queryAvailable(child: PsiElement): Collection<QueryElement.QueryResult> {
         val insertStmt = (this.parent as InsertStmtMixin)
         val tableName = insertStmt.tableName
         val table = tablesAvailable(this).first { it.tableName.name == tableName.name }.query
 
-        if (child is UpsertConflictTarget) {
+        if (child is SqliteUpsertConflictTarget) {
             return super.queryAvailable(child)
         }
 
-        if (child is UpsertDoUpdate) {
+        if (child is SqliteUpsertDoUpdate) {
             val excludedTable = QueryElement.QueryResult(
                 SingleRow(
                     tableName, "excluded"
