85845a3e30ef5f47d881aedaf4db38b7e4e2986f
Matthew Haughton <3flex@users.noreply.github.com>
2021-02-22 23:20:16 +1100

    Tiny deprecation fix

--- core/src/main/kotlin/com/alecstrong/sql/psi/core/SqlParserDefinition.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/SqlParserDefinition.kt
@@ -24,7 +24,7 @@ abstract class SqlParserDefinition : ParserDefinition {
   override fun getWhitespaceTokens() = WHITE_SPACES
   override fun getCommentTokens() = COMMENTS
   override fun getStringLiteralElements(): TokenSet = TokenSet.EMPTY
-  override fun spaceExistanceTypeBetweenTokens(p0: ASTNode, p1: ASTNode) = MAY
+  override fun spaceExistenceTypeBetweenTokens(p0: ASTNode, p1: ASTNode) = MAY
   override fun createElement(node: ASTNode): PsiElement = SqlParserUtil.createElement(node)
 
   override fun createParser(project: Project) = SqlParser()
