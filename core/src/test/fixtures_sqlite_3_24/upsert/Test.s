5848d976df3979be4453d35b5663485ac19fd79a
Alec Strong <astrong@squareup.com>
2021-04-22 13:19:59 -0400

    Protect against keywords that will fail at runtime

--- core/src/test/fixtures_sqlite_3_24/upsert/Test.s
+++ core/src/test/fixtures_sqlite_3_24/upsert/Test.s
@@ -1,69 +1,69 @@
-CREATE TABLE transaction(
-    transaction_id INTEGER PRIMARY KEY,
-    transaction_uuid TEXT NOT NULL UNIQUE,
+CREATE TABLE test(
+    test_id INTEGER PRIMARY KEY,
+    test_uuid TEXT NOT NULL UNIQUE,
     opened_time TEXT NOT NULL,
     finalized_time TEXT,
     count INTEGER
 );
 
-INSERT INTO transaction(transaction_uuid, opened_time, finalized_time)
+INSERT INTO test(test_uuid, opened_time, finalized_time)
 VALUES (?, ?, ?)
-ON CONFLICT (transaction_uuid)
+ON CONFLICT (test_uuid)
 DO UPDATE SET opened_time = excluded.opened_time, finalized_time = excluded.finalized_time;
 
-INSERT INTO transaction(transaction_uuid, opened_time, finalized_time)
+INSERT INTO test(test_uuid, opened_time, finalized_time)
 VALUES (?, ?, ?)
-ON CONFLICT (transaction_uuid)
+ON CONFLICT (test_uuid)
 DO UPDATE SET finalized_time = excluded.finalized_time
 WHERE finalized_time IS NULL;
 
-INSERT INTO transaction(transaction_uuid, opened_time, finalized_time)
+INSERT INTO test(test_uuid, opened_time, finalized_time)
 VALUES (?, ?, ?)
-ON CONFLICT (transaction_uuid) DO NOTHING;
+ON CONFLICT (test_uuid) DO NOTHING;
 
-INSERT INTO transaction(transaction_uuid, opened_time, finalized_time)
+INSERT INTO test(test_uuid, opened_time, finalized_time)
 VALUES (?, ?, ?)
 ON CONFLICT DO NOTHING;
 
 -- SET clause should be able to access CTE
 WITH t(foo) AS (VALUES (?))
-INSERT INTO transaction(transaction_uuid, opened_time, finalized_time)
+INSERT INTO test(test_uuid, opened_time, finalized_time)
 VALUES (?, ?, ?)
-ON CONFLICT (transaction_uuid)
+ON CONFLICT (test_uuid)
 DO UPDATE SET opened_time = excluded.opened_time, finalized_time = (SELECT foo FROM t);
 
--- Conflict list should only be able to access the transaction table, not the CTE
+-- Conflict list should only be able to access the test test, not the CTE
 WITH t(foo) AS (VALUES(?))
-INSERT INTO transaction(transaction_uuid, opened_time, finalized_time)
+INSERT INTO test(test_uuid, opened_time, finalized_time)
 VALUES (?, ?, ?)
 ON CONFLICT (foo) DO NOTHING;
 
 -- If specifying a conflict resolution strategy like REPLACE, the ON CONFLICT must be DO NOTHING
-INSERT OR REPLACE INTO transaction(transaction_uuid, opened_time, finalized_time)
+INSERT OR REPLACE INTO test(test_uuid, opened_time, finalized_time)
 VALUES (?, ?, ?)
-ON CONFLICT (transaction_uuid) DO NOTHING;
+ON CONFLICT (test_uuid) DO NOTHING;
 
 -- Cannot use DO UPDATE when OR REPLACE conflict resolution algorithm was specified
-INSERT OR REPLACE INTO transaction(transaction_uuid, opened_time, finalized_time)
+INSERT OR REPLACE INTO test(test_uuid, opened_time, finalized_time)
 VALUES (?, ?, ?)
-ON CONFLICT (transaction_uuid) DO UPDATE SET opened_time = excluded.opened_time;
+ON CONFLICT (test_uuid) DO UPDATE SET opened_time = excluded.opened_time;
 
 -- Should pass
-INSERT INTO transaction(transaction_uuid, opened_time, finalized_time, count)
+INSERT INTO test(test_uuid, opened_time, finalized_time, count)
 VALUES (?, ?, ?, ?)
-ON CONFLICT (transaction_uuid) DO UPDATE SET count = transaction.count + excluded.count;
+ON CONFLICT (test_uuid) DO UPDATE SET count = test.count + excluded.count;
 
 -- Should fail
-INSERT INTO transaction(transaction_uuid, opened_time, finalized_time, count)
+INSERT INTO test(test_uuid, opened_time, finalized_time, count)
 VALUES (?, ?, ?, ?)
-ON CONFLICT (transaction_uuid) DO UPDATE SET count = transaction_alias.count + excluded.count;
+ON CONFLICT (test_uuid) DO UPDATE SET count = test_alias.count + excluded.count;
 
 -- Should fail
-INSERT INTO transaction AS transaction_alias(transaction_uuid, opened_time, finalized_time, count)
+INSERT INTO test AS test_alias(test_uuid, opened_time, finalized_time, count)
 VALUES (?, ?, ?, ?)
-ON CONFLICT (transaction_uuid) DO UPDATE SET count = transaction.count + excluded.count;
+ON CONFLICT (test_uuid) DO UPDATE SET count = test.count + excluded.count;
 
 -- Should pass
-INSERT INTO transaction AS transaction_alias(transaction_uuid, opened_time, finalized_time, count)
+INSERT INTO test AS test_alias(test_uuid, opened_time, finalized_time, count)
 VALUES (?, ?, ?, ?)
-ON CONFLICT (transaction_uuid) DO UPDATE SET count = transaction_alias.count + excluded.count;
+ON CONFLICT (test_uuid) DO UPDATE SET count = test_alias.count + excluded.count;
