d0a42cbc9433b20bc6b2f7b2d511fd412d1a26e5
Kevin Cianfarini <kevincianfarini@gmail.com>
2021-09-15 12:10:11 -0400

    Include `TIMESTAMP WITH TIME ZONE` and `TIMESTAMPTZ`

--- core/src/test/fixtures_postgresql/column_types/Sample.s
+++ core/src/test/fixtures_postgresql/column_types/Sample.s
@@ -25,9 +25,26 @@ CREATE TABLE all_types(
   some_character_varying CHARACTER VARYING(16),
   some_varchar VARCHAR(16),
   some_text TEXT,
+
   some_date DATE,
+
   some_time TIME,
+  some_precision_time TIME(3),
+  some_time_without_timezone TIME WITHOUT TIME ZONE,
+  some_time_precision_without_timezone TIME(3) WITHOUT TIME ZONE,
+  some_time_with_time_zone TIME WITH TIME ZONE,
+  some_precision_time_with_time_zone TIME(3) WITH TIME ZONE,
+
+
   some_timestamp TIMESTAMP,
+  some_timestamp_with_precision TIMESTAMP (3),
+  some_timestamp_with_timezone TIMESTAMP WITH TIME ZONE,
+  some_timestamp_without_timezone TIMESTAMP WITHOUT TIME ZONE,
+  some_timestamp_precision_with_timezone TIMESTAMP (3) WITH TIME ZONE,
+  some_timestamp_precision_without_timezone TIMESTAMP (3) WITHOUT TIME ZONE,
+
+  some_timestamptz_shorthand TIMESTAMPTZ,
+
   some_boolean BOOLEAN,
   some_bool BOOL,
   some_json JSON,
