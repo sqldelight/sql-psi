9cee0226bb1a4af40a5d50a6e85f0a9643de8f69
Alec Strong <astrong@squareup.com>
2022-03-25 13:17:44 -0400

    Move SQLite 3.18 dialect to SQLDelight

---
+++ core/src/test/kotlin/com/alecstrong/sql/psi/core/AnsiSqlTests.kt
@@ -0,0 +1,30 @@
+package com.alecstrong.sql.psi.core
+
+import com.alecstrong.sql.psi.test.fixtures.FixturesTest
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+import java.io.File
+
+@RunWith(Parameterized::class)
+class AnsiSqlTests(name: String, fixtureRoot: File) : FixturesTest(name, fixtureRoot) {
+  override val replaceRules = arrayOf(
+    "?1" to "?",
+    "?2" to "?",
+    ":searchText" to "?",
+    ":bind" to "?",
+    ":bufferId" to "?",
+    ":ignored" to "?",
+    "AUTOINCREMENT" to "DEFAULT 0",
+  )
+
+  override fun setupDialect() {
+    // No-op.
+  }
+
+  companion object {
+    @Suppress("unused") // Used by Parameterized JUnit runner reflectively.
+    @Parameters(name = "{0}")
+    @JvmStatic fun parameters() = ansiFixtures
+  }
+}
