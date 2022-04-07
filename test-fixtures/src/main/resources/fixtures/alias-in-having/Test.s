66b9bf8464d14d8444e28d15830d4f7080dbb7ea
Alec Strong <astrong@squareup.com>
2022-04-07 18:11:19 -0400

    Pass the projection columns to the group by/having statements

---
+++ test-fixtures/src/main/resources/fixtures/alias-in-having/Test.s
@@ -0,0 +1,12 @@
+-- Table definition edited down to the relevant columns:
+CREATE TABLE `models` (
+  `model_id` TEXT NOT NULL AUTOINCREMENT,
+  `model_descriptor_id` TEXT NOT NULL,
+  `build_date` TEXT NOT NULL,
+  PRIMARY KEY (`model_id`)
+);
+
+SELECT max(build_date) AS build_date_seconds
+FROM models
+GROUP BY model_descriptor_id
+HAVING build_date_seconds > 0 LIMIT 1;
