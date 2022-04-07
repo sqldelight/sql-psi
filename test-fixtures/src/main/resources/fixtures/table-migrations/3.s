d98808863c61774ec327a86bce3eb90c15a64047
Alec Strong <astrong@squareup.com>
2022-04-07 17:06:28 -0400

    Swap double quotes for single quotes

--- test-fixtures/src/main/resources/fixtures/table-migrations/3.s
+++ test-fixtures/src/main/resources/fixtures/table-migrations/3.s
@@ -1,8 +1,8 @@
 -- error[col 12]: No table found with name new_test
 INSERT INTO new_test
-VALUES ("hello", "world");
+VALUES ('hello', 'world');
 
 ALTER TABLE test RENAME TO new_test;
 
 INSERT INTO new_test(id, new_column)
-VALUES ("hello", "world");
+VALUES ('hello', 'world');
