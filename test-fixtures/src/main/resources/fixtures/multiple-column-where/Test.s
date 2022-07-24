492bcd09e92d6ee76258c2af7f5ba4eac0d1bf98
Niclas <niclas.kroon@protonmail.ch>
2022-07-24 18:02:11 +0200

    add multiple column where fixture

---
+++ test-fixtures/src/main/resources/fixtures/multiple-column-where/Test.s
@@ -0,0 +1,12 @@
+CREATE TABLE posts (
+  id TEXT NOT NULL PRIMARY KEY,
+  text TEXT,
+  created_at INTEGER NOT NULL
+);
+
+-- works fine.
+SELECT *
+FROM posts
+WHERE (id, created_at) <= (?, ?)
+ORDER BY created_at DESC
+LIMIT 4;
