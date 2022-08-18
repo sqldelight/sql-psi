3de91308050aa802e3a26fc3af735d48608455c9
hfhbd <hfhbd@users.noreply.github.com>
2022-08-18 18:30:38 +0200

    Use CompoundSelect instead

--- core/src/main/kotlin/com/alecstrong/sql/psi/core/psi/mixins/SetStmtMixin.kt
+++ core/src/main/kotlin/com/alecstrong/sql/psi/core/psi/mixins/SetStmtMixin.kt
@@ -1,40 +1,31 @@
 package com.alecstrong.sql.psi.core.psi.mixins
 
 import com.alecstrong.sql.psi.core.SqlAnnotationHolder
-import com.alecstrong.sql.psi.core.psi.FromQuery
+import com.alecstrong.sql.psi.core.psi.*
 import com.alecstrong.sql.psi.core.psi.QueryElement.QueryResult
-import com.alecstrong.sql.psi.core.psi.SqlCompositeElementImpl
-import com.alecstrong.sql.psi.core.psi.SqlSetStmt
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 
 internal abstract class SetStmtMixin(
   node: ASTNode,
-) : SqlCompositeElementImpl(node),
-  SqlSetStmt,
-  FromQuery {
+) : WithClauseContainer(node),
+  SqlSetStmt {
 
   override fun queryAvailable(child: PsiElement): Collection<QueryResult> {
-    val selectStmt = selectStmt ?: return emptyList()
-    return if (child in selectStmt.children) {
-      selectStmt.queryAvailable(child)
+    val compoundSelectStmt = compoundSelectStmt ?: return exprList.map {
+      QueryResult(it)
+    }
+    return if (child in compoundSelectStmt.children) {
+      compoundSelectStmt.queryAvailable(child)
     } else emptyList()
   }
 
-  override fun queryExposed(): Collection<QueryResult> {
-    val selectStmt = selectStmt ?: return emptyList()
-    return selectStmt.queryExposed()
-  }
+  override fun getWithClause(): SqlWithClause? = compoundSelectStmt?.withClause
 
-  override fun fromQuery(): Collection<QueryResult> {
-    selectStmt?.joinClause?.let {
-      return it.queryExposed()
-    }
-    return emptyList()
-  }
+  override fun queryExposed(): Collection<QueryResult> = compoundSelectStmt?.queryExposed() ?: emptyList()
 
   override fun annotate(annotationHolder: SqlAnnotationHolder) {
     super.annotate(annotationHolder)
-    selectStmt?.annotate(annotationHolder)
+    compoundSelectStmt?.annotate(annotationHolder)
   }
 }
