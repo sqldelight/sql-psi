80f0a565ca2f4623281685965caa15a9ad6d934c
hfhbd <hfhbd@users.noreply.github.com>
2023-06-07 14:02:23 +0200

    Add test

--- sample-headless/src/main/kotlin/com/alecstrong/sql/psi/sample/headless/SampleHeadlessParser.kt
+++ sample-headless/src/main/kotlin/com/alecstrong/sql/psi/sample/headless/SampleHeadlessParser.kt
@@ -1,13 +1,14 @@
 package com.alecstrong.sql.psi.sample.headless
 
 import com.alecstrong.sql.psi.core.SqlCoreEnvironment
+import com.alecstrong.sql.psi.sample.core.SampleFile
 import com.alecstrong.sql.psi.sample.core.SampleFileType
 import com.alecstrong.sql.psi.sample.core.SampleParserDefinition
 import com.intellij.psi.PsiDocumentManager
 import java.io.File
 
 class SampleHeadlessParser {
-  fun parseSqlite(sourceFolders: List<File>, onError: (String) -> Unit) {
+  fun parseSqlite(sourceFolders: List<File>, onError: (String) -> Unit): List<SampleFile> {
     val parserDefinition = SampleParserDefinition()
     val environment = object : SqlCoreEnvironment(
       sourceFolders = sourceFolders,
@@ -19,12 +20,20 @@ class SampleHeadlessParser {
           registerParserDefinition(parserDefinition)
         }
       }
+
       val project = projectEnvironment.project
     }
     environment.annotate { element, message ->
       val file = PsiDocumentManager.getInstance(environment.project).getDocument(element.containingFile)!!
-      val error = "${element.containingFile.virtualFile.path}: (${file.getLineNumber(element.textOffset) + 1}, ${element.textOffset - file.getLineStartOffset(file.getLineNumber(element.textOffset))}): $message"
+      val error = "${element.containingFile.virtualFile.path}: (${file.getLineNumber(element.textOffset) + 1}, ${
+        element.textOffset - file.getLineStartOffset(file.getLineNumber(element.textOffset))
+      }): $message"
       onError(error)
+    }
+    return buildList {
+      environment.forSourceFiles<SampleFile> {
+        add(it)
+      }
     }
   }
 }
--- sample-headless/src/test/kotlin/com/alecstrong/sql/psi/sample/headless/SampleHeadlessParserTest.kt
+++ sample-headless/src/test/kotlin/com/alecstrong/sql/psi/sample/headless/SampleHeadlessParserTest.kt
@@ -1,14 +1,44 @@
 package com.alecstrong.sql.psi.sample.headless
 
-import org.junit.Assert
-import org.junit.Test
+import com.alecstrong.sql.psi.core.psi.SqlLiteralExpr
+import com.alecstrong.sql.psi.sample.core.psi.CustomExpr
+import com.intellij.psi.util.childrenOfType
 import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class SampleHeadlessParserTest {
   @Test
   fun parserIsSuccessful() {
-    SampleHeadlessParser().parseSqlite(listOf(File("../sample-headless"))) {
-      Assert.fail(it)
+    val files = SampleHeadlessParser().parseSqlite(listOf(File("../sample-headless"))) {
+      fail(it)
+    }
+    for (file in files) {
+      val stmts = file.sqlStmtList ?: continue
+      for (stmt in stmts.stmtList) {
+        when {
+          stmt.createTableStmt != null -> {
+            val createTableStmt = stmt.createTableStmt!!
+            for (columnDef in createTableStmt.columnDefList) {
+              val literalExpr = columnDef.childrenOfType<SqlLiteralExpr>().single()
+              assertEquals(42, literalExpr.literalValue.numericLiteral!!.text.toInt())
+            }
+          }
+
+          stmt.compoundSelectStmt != null -> {
+            val select = stmt.compoundSelectStmt!!.selectStmtList.single()
+            val exprs = select.exprList
+            for (expr in exprs) {
+              if (expr is CustomExpr) {
+                val fooRule = expr.fooRule
+                val literalExpr = fooRule.childrenOfType<SqlLiteralExpr>().single().childrenOfType<SqlLiteralExpr>().single()
+                assertEquals(13, literalExpr.literalValue.numericLiteral!!.text.toInt())
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
