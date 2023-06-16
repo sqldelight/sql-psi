ef2b5f4e797c4c5590c4ebe13de988dcc6967193
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-06-16 12:58:59 +0200

    Move custom expr to top (#517)
    * Allow overriding all expr by moving other_expr to the top
    
    * Add test
    
    ---------
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

---
+++ sample-headless/src/main/kotlin/com/alecstrong/sql/psi/sample/headless/SampleHeadlessParser.kt
@@ -0,0 +1,45 @@
+package com.alecstrong.sql.psi.sample.headless
+
+import com.alecstrong.sql.psi.core.SqlCoreEnvironment
+import com.alecstrong.sql.psi.sample.core.SampleFile
+import com.alecstrong.sql.psi.sample.core.SampleFileType
+import com.alecstrong.sql.psi.sample.core.SampleParserDefinition
+import com.intellij.psi.PsiDocumentManager
+import java.io.File
+
+class SampleHeadlessParser {
+  fun parseSqlite(sourceFolders: List<File>, onError: (String) -> Unit): List<SampleFile> {
+    val parserDefinition = SampleParserDefinition()
+    val environment = object : SqlCoreEnvironment(
+      sourceFolders = sourceFolders,
+      dependencies = emptyList(),
+    ) {
+      init {
+        initializeApplication {
+          registerFileType(SampleFileType, SampleFileType.defaultExtension)
+          registerParserDefinition(parserDefinition)
+        }
+      }
+
+      val project = projectEnvironment.project
+    }
+    environment.annotate { element, message ->
+      val file = PsiDocumentManager.getInstance(environment.project).getDocument(element.containingFile)!!
+      val error = "${element.containingFile.virtualFile.path}: (${file.getLineNumber(element.textOffset) + 1}, ${
+        element.textOffset - file.getLineStartOffset(file.getLineNumber(element.textOffset))
+      }): $message"
+      onError(error)
+    }
+    return buildList {
+      environment.forSourceFiles<SampleFile> {
+        add(it)
+      }
+    }
+  }
+}
+
+fun main() {
+  SampleHeadlessParser().parseSqlite(listOf(File("sample-headless"))) {
+    System.err.println(it)
+  }
+}
