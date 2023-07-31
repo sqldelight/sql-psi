e68a628de3392dd49633dd63ba76a5fb4e08788a
Adriel Martinez <adriel.martinez@faire.com>
2023-07-31 11:42:51 -0400

    Remove support for multiple values. Add support for compound select.

--- test-fixtures/src/main/resources/fixtures/multi-column-select-in-where/Test.s
+++ test-fixtures/src/main/resources/fixtures/multi-column-select-in-where/Test.s
@@ -1,12 +1,20 @@
 CREATE TABLE foo(
   id INTEGER NOT NULL,
-  name TEXT NOT NULL
+  col_one TEXT NOT NULL,
+  col_two TEXT NOT NULL
 );
 
-SELECT *
-FROM foo
-WHERE (id, name) IN ?;
+CREATE TABLE bar(
+  id INTEGER NOT NULL,
+  x TEXT NOT NULL,
+  y TEXT NOT NULL
+);
+
 
 SELECT *
 FROM foo
-WHERE (id, name) IN (?, ?);
+WHERE (id, col_one) IN ?;
+
+SELECT *
+FROM foo
+WHERE (col_one, col_two) IN ( SELECT x, y FROM bar );
