736bb836dcf19934bbb0b1da9c4ea56f56513b0a
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-04-05 14:57:15 +0200

    Fix join (#618)

--- core/src/testFixtures/resources/fixtures/join-with-using-clause/Test.s
+++ core/src/testFixtures/resources/fixtures/join-with-using-clause/Test.s
@@ -17,3 +17,15 @@ WHERE a_id = 1;
 SELECT B.a_id
 FROM A
 JOIN B USING (a_id);
+
+SELECT B.a_id
+FROM A
+LEFT JOIN B USING (a_id);
+
+SELECT B.a_id
+FROM A
+RIGHT JOIN B USING (a_id);
+
+SELECT B.a_id
+FROM A
+FULL JOIN B USING (a_id);
