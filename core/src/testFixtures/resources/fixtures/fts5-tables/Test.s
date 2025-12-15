1cf966cd99c63fa35c45a7b5a500ca6f30217249
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-12-15 11:25:09 +0000

    Update Test.s
    Add some examples of numeric literal arguments

--- core/src/testFixtures/resources/fixtures/fts5-tables/Test.s
+++ core/src/testFixtures/resources/fixtures/fts5-tables/Test.s
@@ -1,3 +1,8 @@
+CREATE VIRTUAL TABLE ft_locale USING fts5(a, b, c, locale=1);
+CREATE VIRTUAL TABLE ft_columnsize USING fts5(a, b, c, columnsize=0);
+CREATE VIRTUAL TABLE ft_contentless_delete USING fts5(a, b, c, content='', contentless_delete=1);
+CREATE VIRTUAL TABLE ft_contentless_unindexed USING fts5(a, b UNINDEXED, content='', contentless_unindexed=1);
+
 CREATE VIRTUAL TABLE data5 USING fts5(text, content=other_table, content_rowid=rowid, prefix='2 3 4 5 6');
 
 SELECT *
